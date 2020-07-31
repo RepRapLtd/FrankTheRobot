f0c25b72116ab6145ae9ddf1982976c1d5c44ef5
Adrian Bowyer <adrian@reprappro.com>
2020-07-31 16:55:13 +0100

    Improved motor test program WIP.

--- Frank 2.0/Software/opto-test/opto-test.ino
+++ Frank 2.0/Software/opto-test/opto-test.ino
@@ -13,12 +13,27 @@ const int modulatePin = 13;
 const int sensePin = A0;
 const int pwmA = 5;
 const int pwmB = 6;
-const int stopped = 0;
-const int forward = 1;
-const int backward = 2;
+const int factor255 = 17;
+const int stabiliseTime = 100;
+const long sampleTime = 5000;
+
+enum condition {stopped, forward, backward};
+condition state = stopped;
+
+enum stage {inactive, fStarting, fRising, fFalling, fStopping, rStarting, rRising, rFalling, rStopping};
+stage test = inactive;
+long testTime;
+bool sensor = false;
+int pulseCount = 0;
+bool gRun = false;
+
 
 int pwm = 200;
-int state = stopped;
+int oldPWM = 200;
+bool graphLive = false;
+long graphTime = 0;
+int trough = 1024;
+int peak = 0;
 
 void PrintInt(int v)
 {
@@ -64,9 +79,174 @@ int OptoReading(bool printBar)
     int stars = vDiff/10;
     for (int i = 0; i < stars; i++)
       Serial.print('*');
-    Serial.println();    
+    Serial.println();
+    if(vDark > peak)
+      peak = vDark;
+    if(vLight < trough)
+      trough = vLight;    
   }
   return vDiff;  
+}
+
+void Prompt()
+{
+  Serial.println("\nCommands:");
+  Serial.println(" f: Go forwards.");
+  Serial.println(" b: Go backwards.");
+  Serial.println(" s: Stop.");
+  Serial.println(" pNNN: Set PWM to NNN.");
+  Serial.println(" g: Plot sensor readings for 10 seconds and set peak and trough values.");
+  Serial.println(" c: Output PWM/RPM curves.\n");
+}
+
+void Timings()
+{
+  if(graphLive)
+  {
+    long t = millis();
+    if(t - 10000 >= graphTime)
+    {
+      graphLive = false;
+      Serial.print("\nTrough (light): ");
+      Serial.print(trough);
+      Serial.print(", Peak (dark): ");
+      Serial.print(peak);
+      Serial.println('\n');
+    }       
+  }
+}
+
+
+bool Trigger()
+{
+  bool s = sensor;
+  int reading = OptoReading(false);
+  if(reading < (peak - trough)/3)
+    sensor = false;
+  if(reading > ((peak - trough)*2)/3)
+    sensor = true;
+  return s ^ sensor; 
+}
+
+void TurnOnOrOff()
+{
+  switch (state)
+  {
+    case forward:
+      Forward();
+      break;
+    case backward:
+      Backward();
+      break;
+    case stopped:
+      Stop();
+      break;
+    default:
+      ;
+  }
+  delay(stabiliseTime);
+  testTime = millis();
+}
+
+bool TestAtSpeed()
+{
+  if(Trigger())
+    pulseCount++;
+  long t = millis();
+  if(t - testTime > sampleTime)
+  {
+    float rpm = 60000.0*(float)pulseCount/((float)sampleTime*24.0);
+    Serial.print(' ');
+    Serial.print(pwm);
+    Serial.print(", ");
+    Serial.println(rpm);
+    testTime = t;
+    pulseCount = 0;
+    return true;
+  }
+  return false;
+}
+
+void Test()
+{
+  switch(test)
+  {
+    case fStarting:
+      oldPWM = pwm;
+      Serial.println("\nPWM/speed test");
+    case rStarting:
+      pwm = 0;
+      Stop();
+      delay(stabiliseTime);
+      if(test == fStarting)
+      {
+        Forward();
+        Serial.print("Forward");
+        test = fRising;
+      } else
+      {
+        Backward();
+        Serial.print("Backward");
+        test = rRising;
+      }
+      Serial.println(" rising curve:");
+      Serial.println(" PWM, RPM");
+      TurnOnOrOff();
+      pulseCount = 0;
+      Trigger();      
+      break;
+
+    case fRising:
+    case rRising:
+      if(TestAtSpeed())
+      {
+        pwm += factor255;
+        if(pwm > 255)
+        {
+          pwm = 255;
+          Serial.println(" falling curve:");
+          Serial.println(" PWM, RPM");
+          if(test == fRising)
+            test = fFalling;
+          else
+            test = rFalling;
+        } else
+          TurnOnOrOff();
+      }
+      break;
+
+
+    case fFalling:
+    case rFalling:
+      if(TestAtSpeed())
+      {
+        pwm -= factor255;
+        if(pwm < 0)
+        {
+          pwm = 0;
+          Serial.println(" falling curve:");
+          Serial.println(" PWM, RPM");
+          if(test == fRising)
+            test = fFalling;
+          else
+            test = rFalling;
+        } else
+          TurnOnOrOff();
+      }
+      break;
+
+    case fStopping:
+      test = rStarting;
+      break;
+      
+    case rStopping:
+      pwm = oldPWM;
+      test = inactive;
+      break;
+
+    default:
+    ;
+  }
 }
 
 void setup() 
@@ -83,9 +263,16 @@ void setup()
 
 void loop() 
 {
-  int v = OptoReading(true); 
-  delay(30);
-  if(Serial.available() > 0)
+  Timings();
+
+  if(test != inactive)
+  {
+    Test();
+  }else if(graphLive)
+  {
+    int v = OptoReading(true); 
+    delay(30);
+  } else if(Serial.available() > 0)
   {
     int c = Serial.read();
     switch(c)
@@ -101,25 +288,27 @@ void loop()
         break;
       case 'p':
         pwm = Serial.parseInt();
-        switch (state)
-        {
-          case forward:
-            Forward();
-            break;
-          case backward:
-            Backward();
-            break;
-          case stopped:
-            Stop();
-            break;
-          default:
-            ;
-        }
+        TurnOnOrOff();
+        break;
+      case 'g':
+        graphLive = true;
+        trough = 1024;
+        peak = 0;
+        graphTime = millis();
+        gRun = true;
+        break;
+      case 'c':
+        if(gRun)
+          test = fStarting;
+        else
+          Serial.println("You need to run a g test before a c test to set the opto range.");
         break;
       case '\n':
         break;
       default:
-        Serial.println("Valid inputs: f, b, s, pnnn.");
+        Serial.print("\nUnrecognised command: ");
+        Serial.println((char)c);
+        Prompt();
     }
   }
 }
